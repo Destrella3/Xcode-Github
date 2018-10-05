9de996bb8d14df3c0cec4af1c3a12bba8485a12e
Destrella3 <destrella385@gmail.com>
2018-10-05 14:34:42 -0400

    Initial Commit

---
+++ Xcode-Github/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Xcode-Github
+//
+//  Created by Diego Estrella III on 10/5/18.
+//  Copyright © 2018 Diego Estrella III. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
